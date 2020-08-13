0127f03ce2b895b31245efed384f317d1cdc693f
Josh Raiborde <joshsrai@yahoo.com>
2020-08-13 22:17:37 +0000

    Automatically backed up by Learn

--- lib/car.rb
+++ lib/car.rb
@@ -1,1 +1,11 @@
 require_relative "./vehicle.rb"
+
+class Car < Vehicle 
+  
+  def initialize(wheel_size, wheel_number)
+    @wheel_size = wheel_size
+    @wheel_number = wheel_number
+  end
+  
+  
+end
